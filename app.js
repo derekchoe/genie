261d0b2152b3158ed99dd8a57a6e3336beeabe66
Xiaoyun Yang <xiaoyun.ouo@gmail.com>
2018-10-21 20:22:06 -0700

    Add backend user auth

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let app = express();
-const users = require("./routes/api/users");
+const express = require('express');
 const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-mongoose
-  .connect(db)
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+const users = require('./routes/api/users');
+
+const app = express();
+const db = require('./config/keys').mongoURI;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.log(err));
 
-app.use("/api/users", users);
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
