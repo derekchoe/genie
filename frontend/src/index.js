8a361a2000a82ac56aaf880aaca3e510c5942b31
Rika <ruijiasun1323@gmail.com>
2018-10-23 11:11:44 -0700

    Update editCategory

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {
+    fetchCategories,
+    fetchCategory,
+    deleteCategory,
+    createCategory,
+    updateCategory
+} from './action/category_actions';
+import { loginUser } from './action/session_actions';
+import store from './store/store';
+
+// test start
+window.loginUser = user => store.dispatch(loginUser(user));
+window.fetchCategories = () => store.dispatch(fetchCategories());
+window.fetchCategory = id => store.dispatch(fetchCategory(id));
+window.deleteCategory = id => store.dispatch(deleteCategory(id));
+window.createCategory = category => store.dispatch(createCategory(category));
+window.updateCategory = category => store.dispatch(updateCategory(category));
+// Test end
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
