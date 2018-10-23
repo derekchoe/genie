8a4093e5a5ec14ff9884bfcf3c9512531d5f39f9
Xiaoyun Yang <xiaoyun.ouo@gmail.com>
2018-10-22 22:24:29 -0700

    Fix transaction validation bugs

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -6,10 +6,7 @@ const keys = require('../../config/key');
 const passport = require('passport');
 
 const Category = require('../../models/Category');
-<<<<<<< HEAD
-=======
-// const Transaction = require('../../models/Transaction');
->>>>>>> 4bdf4232f2d1c22909675cd4f0e3eb1a9fb9cdf5
+const Transaction = require('../../models/Transaction');
 const validateCategoryInput = require('../../validation/category');
 
 router.get('/test', (req, res) => res.json({ msg: 'Categories works' }));
@@ -28,36 +25,6 @@ router.get(
 );
 
 // return one category with all transactions
-<<<<<<< HEAD
-router.get('/:id', (req, res) => {});
-
-// create categories
-router.post('/',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      const { errors, isValid } = validateCategoryInput(req.body);
-
-      if (!isValid) {
-        return res.status(400).json(errors);
-      }
-
-      const CategoryFields = {};
-      CategoryFields.user = req.user.id;
-      if (req.body.name) CategoryFields.name = req.body.name;
-      if (req.body.description) {
-        CategoryFields.description = req.body.description;
-      }
-      if (req.body.budget) CategoryFields.budget = req.body.budget;
-      new Category(CategoryFields).save().then(category => res.json(category));
-    }
-);
-
-// edit categories
-router.patch('/:id', (req, res) => {});
-
-// delete categories
-router.delete('/', (req, res) => {});
-=======
 router.get(
   '/:id',
   passport.authenticate('jwt', { session: false }),
@@ -127,20 +94,19 @@ router.delete(
   (req, res) => {
     Category.findById(req.params.id)
       .then(category => {
-        // const transactionId = category.transactions;
+        const transactionId = category.transactions;
         category.remove();
-        // Transaction.find({ category: category.id })
-        //   .then(transactions => {
-        //     if (transactions.length !== 0) {
-        //       transactions.forEach(transaction => transaction.remove());
-        //     }
-        //   })
-        //   .catch(err => res.status(404).json(err));
+        Transaction.find({ category: category.id })
+          .then(transactions => {
+            if (transactions.length !== 0) {
+              transactions.forEach(transaction => transaction.remove());
+            }
+          })
+          .catch(err => res.status(404).json(err));
       })
       .then(res.json({ category: 'Category removed' }))
       .catch(err => res.status(404).json(err));
   }
 );
->>>>>>> 4bdf4232f2d1c22909675cd4f0e3eb1a9fb9cdf5
 
 module.exports = router;
