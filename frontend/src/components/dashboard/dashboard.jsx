918d1a9b65a9e3194da7f6fead512fe36be6e956
Rika <ruijiasun1323@gmail.com>
2018-10-24 16:46:06 -0700

    setup sidebar

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import NavbarContainer from '../layout/navbar_container'
+import SidebarContainer from '../sidebar/sidebar_container'
 
 export default class DashBoard extends Component {
   render() {
     return (
       <div>
         <NavbarContainer/>
+        <SidebarContainer/>
         This is the dashboard
       </div>
     )
