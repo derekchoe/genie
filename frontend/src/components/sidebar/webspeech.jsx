b77b00d5dd25fc096d65a49078fcbbf9d9afcb03
Rika <ruijiasun1323@gmail.com>
2018-10-25 15:36:46 -0700

    change VSC setting

--- frontend/src/components/sidebar/webspeech.jsx
+++ frontend/src/components/sidebar/webspeech.jsx
@@ -2,74 +2,74 @@ import React, { Component } from 'react'
 
 export default class WebSpeech extends Component {
     constructor(props) {
-        super(props)
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.state = {stream: false};
-        this.transcript = "";
+			super(props)
+			this.handleSubmit = this.handleSubmit.bind(this);
+			this.state = {stream: false};
+			this.transcript = "";
     }
 
     componentWillUnmount() {
-        // this.props.clearErrors();
+			// this.props.clearErrors();
     }
 
     handleSubmit(e) {
-        e.preventDefault();
-        // this.props.clearErrors();
+			e.preventDefault();
+			// this.props.clearErrors();
 
-        let SpeechRecognition = window.SpeechRecognition 
-        || window.webkitSpeechRecognition;
-        
-        if (!this.state.stream) {
-            this.setState({ stream: true });
-            // console.log(this.transcript);
-            
+			let SpeechRecognition = window.SpeechRecognition 
+			|| window.webkitSpeechRecognition;
+			
+			if (!this.state.stream) {
+					this.setState({ stream: true });
+					// console.log(this.transcript);
+					
 
-            this.recognition = new SpeechRecognition();
-            this.recognition.continuous = true;
-            this.recognition.interimResults = true;
-            this.recognition.lang = "en-US";
-            this.recognition.maxAlternatives = 1
-            
-            let text = document.querySelector(".live-text");
-            let p = document.createElement("p");
-        
-            text.appendChild(p);
+					this.recognition = new SpeechRecognition();
+					this.recognition.continuous = true;
+					this.recognition.interimResults = true;
+					this.recognition.lang = "en-US";
+					this.recognition.maxAlternatives = 1
+					
+					let text = document.querySelector(".live-text");
+					let p = document.createElement("p");
+			
+					text.appendChild(p);
 
-            this.recognition.addEventListener("result", e => {
-                let transcript = Array.from(e.results)
-                .map(result => result[0])
-                .map(result => result.transcript)
-                .join("");
+					this.recognition.addEventListener("result", e => {
+							let transcript = Array.from(e.results)
+							.map(result => result[0])
+							.map(result => result.transcript)
+							.join("");
 
-                p.textContent = transcript;
-                if (e.results[0].isFinal) {
-                    this.transcript += p.textContent + ". ";
+							p.textContent = transcript;
+							if (e.results[0].isFinal) {
+									this.transcript += p.textContent + ". ";
 
-                    p = document.createElement("p");
-                    text.appendChild(p)
-                }
-            });
+									p = document.createElement("p");
+									text.appendChild(p)
+							}
+					});
 
             
             this.recognition.start();
             this.recognition.addEventListener("end", this.recognition.start)
         } else {
-            this.setState({stream: false})
-            this.recognition.stop();
-            this.recognition.removeEventListener("end", this.recognition.start);
+					this.setState({stream: false})
+					this.recognition.stop();
+					this.recognition.removeEventListener("end", this.recognition.start);
 
-            this.recognition = null;
-            // this.props.createTransaction({
-            //     token: this.props.currentUser.token,
-            //     transcript: this.transcript   
-            // })
+					this.recognition = null;
+					// this.props.createTransaction({
+					//     token: this.props.currentUser.token,
+					//     transcript: this.transcript   
+					// })
 
-            this.transcript = "";
-            //will clear everything when you stop
-            let children = Array.from(document.querySelectorAll(".live-text > p"));
-            children.forEach(child => {
-                child.parentNode.removeChild(child);
-            });
+					this.transcript = "";
+					//will clear everything when you stop
+					let children = Array.from(document.querySelectorAll(".live-text > p"));
+					children.forEach(child => {
+							child.parentNode.removeChild(child);
+					});
 
         }
 
@@ -84,14 +84,13 @@ export default class WebSpeech extends Component {
         let buttonContent = this.state.stream ? "Stop" : "Record";
         return (
         <div>
-            <div>Record Transaction</div>
-
-                <div className='live-text'>
-            <button onClick={this.handleSubmit}>
-                {buttonContent}
-            </button>
-                </div>
-        </div>
+					<div>Record Transaction</div>
+					<div className='live-text'>
+						<button onClick={this.handleSubmit}>
+							{buttonContent}
+						</button>
+					</div>
+       </div>
         )
     }
 };
