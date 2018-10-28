679c130bf63a091efff5fff44045eb0b759fa70c
Xiaoyun Yang <xiaoyun.ouo@gmail.com>
2018-10-28 14:13:59 -0700

    Add success message to create category form

--- frontend/src/components/sidebar/create_cate_form.jsx
+++ frontend/src/components/sidebar/create_cate_form.jsx
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+const statusStyle = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
 
 export default class CreateCategoryForm extends Component {
   constructor(props) {
@@ -9,6 +21,16 @@ export default class CreateCategoryForm extends Component {
       budget: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.openSuccessModal = this.openSuccessModal.bind(this);
+    this.closeSuccessModal = this.closeSuccessModal.bind(this);
+  }
+
+  openSuccessModal() {
+    this.setState({ successModalOpen: true });
+  }
+
+  closeSuccessModal() {
+    this.setState({ successModalOpen: false });
   }
 
   handleInput(field) {
@@ -29,12 +51,31 @@ export default class CreateCategoryForm extends Component {
       description: '',
       budget: ''
     });
-    this.props.createCategory(formData);
+    // this.props.createCategory(formData);
+
+    const createPromise = new Promise((resolve, reject) => {
+      resolve(this.props.createCategory(formData));
+    });
+    createPromise.then(() => {
+      this.openSuccessModal();
+      setTimeout(this.closeSuccessModal, 1000);
+    });
   }
 
   render() {
     return (
       <div className="create-form-box-2">
+        <Modal
+          isOpen={this.state.successModalOpen}
+          onRequestClose={this.closeSuccessModal}
+          contentLabel="Success Modal"
+          style={statusStyle}
+        >
+          <div className="success-modal mod-positive">
+            New category has been added!
+          </div>
+        </Modal>
+
         <h3>Create a new category</h3>
         <form onSubmit={this.handleSubmit}>
           <label>
