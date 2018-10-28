5424af024ad27001f263f33a841be25eac074534
Derek Choe <derekchoe@g.ucla.edu>
2018-10-28 15:59:56 -0700

    fix spacing of charts

--- frontend/src/components/graphs/income_vs_expense_chart.jsx
+++ frontend/src/components/graphs/income_vs_expense_chart.jsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import { Pie, PieChart, Tooltip } from "recharts";
+import { Pie, PieChart, Tooltip, Cell } from "recharts";
 
-const IncomeVsExpenseChart = ({ netIncome }) => {
+const IncomeVsExpenseChart = ({ netIncome, transactionByCategory }) => {
   function formatBetter(data) {
-    const result = []
-    data.map(trans => {
+    const result = [];
+    data.forEach(trans => {
       const income = trans.income || 0;
-      const expense = trans.expense || 0;
-      const netIn = income - expense;
-      result.push({ Category: "Remaining Income", Amount: netIn });
-      result.push({ Category: "Expense", Amount: trans.expense });
+      let expense = trans.expense || 0;
+      let netIn = income - expense;
+      if (netIn < 0) {
+        expense = (income - expense) * -1;
+        netIn = 0;
+      }
+      result.push({ Name: "Remaining Income", Amount: netIn });
+      result.push({ Name: "Expense", Amount: expense });
     });
-    return result.slice(0,2)
+    return result.slice(0, 2);
   }
+  const COLORS = ["#00C49F", "#e60000"];
+  const data = formatBetter(netIncome);
 
-  console.log(formatBetter(netIncome))
-  debugger;
+  function formatBetter1(data) {
+    let result = [];
+
+    data.forEach(datum => {
+      result.push({
+        value: datum.totalExpense,
+        name: datum.categoryName
+      });
+    });
+    return result;
+  }
+  const data1 = formatBetter1(transactionByCategory);
+
   return <div>
-      <PieChart width={500} height={300}>
-        <Pie data={formatBetter(netIncome)} nameKey="name" cx="50%" cy="50%" outerRadius={80} fill="#82ca9d" />
+      <PieChart width={800} height={400} z-index={"1"}>
+        <Pie data={data} dataKey="Amount" nameKey="Name" cx={120} cy={200} labelLine={false} innerRadius={80} outerRadius={110} fill="#8884d8">
+          {data.map((entry, index) => (
+            <Cell fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+
+        <Pie data={data1} dataKey="value" nameKey="name" cx="420" cy="200" innerRadius={80} outerRadius={110} fill="#82ca9d" />
+      <Pie data={data1} dataKey="value" nameKey="name" cx="420" cy="200" outerRadius={60} fill="#8884d8" />
         <Tooltip />
       </PieChart>
     </div>;
