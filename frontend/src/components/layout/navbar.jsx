d4e2681f7cbcc603adc5772e39024b17cba37399
Derek Choe <derekchoe@g.ucla.edu>
2018-11-20 12:17:13 -0800

    navbar color

--- frontend/src/components/layout/navbar.jsx
+++ frontend/src/components/layout/navbar.jsx
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 
 class Navbar extends Component {
   render() {
-    return (
-      <nav className="nav-box">
+    return <nav className="nav-box">
         <div className="logo-logout-wrapper">
           <div className="logo" />
           <button className="logout-button" onClick={this.props.logoutUser}>
-            Log Out
+            <i class="fas fa-sign-out-alt" /> Log Out
           </button>
         </div>
-      </nav>
-    );
+      </nav>;
   }
 }
 
